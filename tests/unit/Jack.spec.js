49f80ab25066f9d6f1eac7a897bd3f12c7ceda4c
jjjjjacckk <zwillinge175@gmail.com>
2023-05-22 10:36:07 +0800

    [add] 2 DM SYNC tests

--- tests/unit/Jack.spec.js
+++ tests/unit/Jack.spec.js
@@ -1,24 +1,266 @@
-import { shallowMount } from '@vue/test-utils'
-// import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount, createLocalVue, mount } from '@vue/test-utils'
+import scoring from '@/views/scoring.vue'
 
-// describe('HelloWorld.vue', () => {
-//   it('renders props.msg when passed', () => {
-//     const msg = 'new message'
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: { msg }
-//     })
-//     expect(wrapper.text()).toMatch(msg)
-//   })
-// })
-import Vue from 'vue'
-import App from '../../src/App.vue'
+// mock the Router
+const mockRouter = {
+  push: jest.fn()
+}
+
+// mock the Route
+const mockRoute = {
+  params: {
+    teamid: '-NTd4gODlBQPL9Na1VYr',
+    contestid: '-NTdKbOPAh9F89udkTNB'
+  }
+}
 
 
-describe('App', () => {
-  test('renders the app container', () => {
-    const wrapper = shallowMount(App)
-    expect(wrapper.find('#app').exists()).toBe(true)
+describe('scoring.vue', () => {
+  it('DB SYNC - Score Board', () => {
+    // 用 spyOn 的方式 intercept "beforeMount" 
+    // 來避免 this.$http.get() Undefined 的錯誤
+    jest.spyOn(scoring, 'beforeMount').mockImplementation(() => {
+      console.log('jest.spyOn()')
+    })
+
+    const wrapper = mount(scoring, {
+      stubs: ['router-link', 'router-view'], 
+      mocks: {
+        $router: mockRouter,
+        $route: mockRoute
+      },
+      data() {
+        return {
+          cur_game: 1,
+          id: this.uid,
+          teamid: this.$route.params.teamid,
+          contestid: this.$route.params.contestid,
+          db: 'https://volleague-default-rtdb.firebaseio.com/',
+          users: {
+            // 'uid': {
+            //   StatisticsList: [''],
+            //   authid: '',
+            //   birthday: {},
+            //   name: {},
+            //   position: [],
+            //   teamList: []
+            // }
+          },
+          contestInfo: {
+            contest: '成功盃',
+            date: '',
+            games: [],
+            opponent: 'NCKU CSIE',
+            score: '',
+            gameScore: '',
+            onCourtMem: '',
+            localRecordsRaw: [''],
+            localRecords: ['']
+          },
+          teamInfo:{
+            teamName: '',
+            bulletin: '',
+            awards: [''],
+            members: [],  // name, number, position, uid
+            contestRecords: [{
+              contest: '成功盃',
+              date: '',
+              gameScore: '',
+              key: '',
+              opponent: ''
+            }],
+          },
+          isGameOver: false,
+          isNextGame: false,
+          isCourtMemSet: false, // bind to court member section
+          isOpponentScore: false, // bind to button, '對方得分'
+          positions: ['OH', 'O', 'MB', 'S', 'L'],
+          translateType2Man: {'attackPoint': '攻擊得分', 'blockPoint': '攔網得分','servicePoint': '發球得分',
+                          'attackError': '攻擊失誤', 'tossError': '舉球失誤', 'blockError': '觸網失誤',
+                          'receiveError': '接發失誤', 'serviceError': '發球失誤', 'oppoScore': '對方得分'},
+          translateType2Eng: {'攻擊得分': 'attackPoint', '攔網得分': 'blockPoint','發球得分': 'servicePoint',
+                          '攻擊失誤': 'attackError', '舉球失誤': 'tossError', '觸網失誤': 'blockError',
+                          '接發失誤': 'receiveError', '發球失誤': 'serviceError', '對方得分': 'oppoScore'},
+          records_pushed_raw: [{
+            'ourTeam': {},
+            'placement': [[''], [''], [''], [''], [''], [''], [''], [''], [''], ['']]
+          }],
+          // records_pushed_raw: [
+          //   {
+          //     'ourTeam': {
+          //       '蘇名偉': {name: '蘇名偉', pos: 'OH', number: '27',
+          //                 attackPoint: 1, blockPoint: 0, servicePoint: 0,
+          //                 attackError: 0, tossError: 0, blockError: 0,
+          //                 receiveError: 0, serviceError: 0 },
+          //       'Test7': {name: 'Test7', pos: 'L', number: '88',
+          //                 attackPoint: 0, blockPoint: 0, servicePoint: 0,
+          //                 attackError: 0, tossError: 0, blockError: 1,
+          //                 receiveError: 0, serviceError: 0 },
+          //     }, 
+          //     'placement': [[''], [''], [{'num': 2, 'pos': 'S'}], [''], [''], [''], [''], [''], [''], ['']] // 0~8: 1~9 九號位置 ; 9: touch out
+          //   }
+          // ],
+          // landing: 呈現在表格 --> 1~9 + touch-out
+          records_local: [],
+          // records_local: [{'num': 88, 'name': 'Test7', 'position': 'L', 'record_type': '攻擊得分', 'landing': -1, 'game': 1},
+          //                 {'num': 27, 'name': '蘇名偉', 'position': 'OH', 'record_type': '攔網失誤', 'landing': -1, 'game': 1},
+          //                 {'num': 2, 'name': 'NCKU EE', 'position': 'S', 'record_type': '對方得分', 'landing': 3, 'game': 1}],
+          scoring: {
+            host: {'name': 'NYCU CS', 'winned_game': 0, 'cur_score': 0},
+            opponent: {'name': 'NCKU CSIE', 'winned_game': 0, 'cur_score': 0}
+          },
+          selected_button: {
+            'name': '',
+            'number': -1,
+            'position': '',
+            'record_type': '',
+            'landing': -1,  // 依照 placement 的 index (0~9)
+            'opponent': {'num': -1, 'pos': ''},
+            'game': this.cur_game
+          },
+          selected_members: [{}, {}, {}, {}, {}, {}, {}],     // e.g.:) {'name': '張祐誠', 'number': 22, 'position': 'MB'}
+          // selected_members: [{'name': '張祐誠', 'number': 22, 'position': 'MB'},
+          //                    {'name': '張祐誠', 'number': 22, 'position': 'MB'},
+          //                    {'name': '張祐誠', 'number': 22, 'position': 'MB'},
+          //                    {'name': '張祐誠', 'number': 22, 'position': 'MB'}, 
+          //                    {'name': '張祐誠', 'number': 22, 'position': 'MB'}, 
+          //                    {'name': '張祐誠', 'number': 22, 'position': 'MB'}]
+        }
+      }
+    })
+
+    console.log(wrapper.findComponent('.scores').text())
+    expect(wrapper.findComponent('.scores').text()).toContain('NCKU CSIE')
+    expect(wrapper.findComponent('.scores').text()).toContain('NYCU CS')
   })
-})
 
-// This is jack!
+  it('DB SYNC - Record', async () => {
+    // 用 spyOn 的方式 intercept "beforeMount" 
+    // 來避免 this.$http.get() Undefined 的錯誤
+    jest.spyOn(scoring, 'beforeMount').mockImplementation(() => {
+      console.log('jest.spyOn()')
+    })
+
+    const wrapper = mount(scoring, {
+      stubs: ['router-link', 'router-view'], 
+      mocks: {
+        $router: mockRouter,
+        $route: mockRoute
+      },
+      data() {
+        return {
+          cur_game: 1,
+          id: this.uid,
+          teamid: this.$route.params.teamid,
+          contestid: this.$route.params.contestid,
+          db: 'https://volleague-default-rtdb.firebaseio.com/',
+          users: {
+            // 'uid': {
+            //   StatisticsList: [''],
+            //   authid: '',
+            //   birthday: {},
+            //   name: {},
+            //   position: [],
+            //   teamList: []
+            // }
+          },
+          contestInfo: {
+            contest: '成功盃',
+            date: '',
+            games: [],
+            opponent: 'NCKU CSIE',
+            score: '',
+            gameScore: '',
+            onCourtMem: '',
+            localRecordsRaw: [''],
+            localRecords: ['']
+          },
+          teamInfo:{
+            teamName: '',
+            bulletin: '',
+            awards: [''],
+            members: [],  // name, number, position, uid
+            contestRecords: [{
+              contest: '成功盃',
+              date: '',
+              gameScore: '',
+              key: '',
+              opponent: ''
+            }],
+          },
+          isGameOver: false,
+          isNextGame: false,
+          isCourtMemSet: false, // bind to court member section
+          isOpponentScore: false, // bind to button, '對方得分'
+          positions: ['OH', 'O', 'MB', 'S', 'L'],
+          translateType2Man: {'attackPoint': '攻擊得分', 'blockPoint': '攔網得分','servicePoint': '發球得分',
+                          'attackError': '攻擊失誤', 'tossError': '舉球失誤', 'blockError': '觸網失誤',
+                          'receiveError': '接發失誤', 'serviceError': '發球失誤', 'oppoScore': '對方得分'},
+          translateType2Eng: {'攻擊得分': 'attackPoint', '攔網得分': 'blockPoint','發球得分': 'servicePoint',
+                          '攻擊失誤': 'attackError', '舉球失誤': 'tossError', '觸網失誤': 'blockError',
+                          '接發失誤': 'receiveError', '發球失誤': 'serviceError', '對方得分': 'oppoScore'},
+          records_pushed_raw: [{
+            'ourTeam': {},
+            'placement': [[''], [''], [''], [''], [''], [''], [''], [''], [''], ['']]
+          }],
+          // records_pushed_raw: [
+          //   {
+          //     'ourTeam': {
+          //       '蘇名偉': {name: '蘇名偉', pos: 'OH', number: '27',
+          //                 attackPoint: 1, blockPoint: 0, servicePoint: 0,
+          //                 attackError: 0, tossError: 0, blockError: 0,
+          //                 receiveError: 0, serviceError: 0 },
+          //       'Test7': {name: 'Test7', pos: 'L', number: '88',
+          //                 attackPoint: 0, blockPoint: 0, servicePoint: 0,
+          //                 attackError: 0, tossError: 0, blockError: 1,
+          //                 receiveError: 0, serviceError: 0 },
+          //     }, 
+          //     'placement': [[''], [''], [{'num': 2, 'pos': 'S'}], [''], [''], [''], [''], [''], [''], ['']] // 0~8: 1~9 九號位置 ; 9: touch out
+          //   }
+          // ],
+          // landing: 呈現在表格 --> 1~9 + touch-out
+          // records_local: [],
+          records_local: [{'num': 88, 'name': 'Test7', 'position': 'L', 'record_type': '攻擊得分', 'landing': -1, 'game': 1},
+                          {'num': 27, 'name': '蘇名偉', 'position': 'OH', 'record_type': '攔網失誤', 'landing': -1, 'game': 1},
+                          {'num': 2, 'name': 'NCKU CSIE', 'position': 'S', 'record_type': '對方得分', 'landing': 3, 'game': 1}],
+          scoring: {
+            host: {'name': 'NYCU CS', 'winned_game': 0, 'cur_score': 0},
+            opponent: {'name': 'NCKU CSIE', 'winned_game': 0, 'cur_score': 0}
+          },
+          selected_button: {
+            'name': '',
+            'number': -1,
+            'position': '',
+            'record_type': '',
+            'landing': -1,  // 依照 placement 的 index (0~9)
+            'opponent': {'num': -1, 'pos': ''},
+            'game': this.cur_game
+          },
+          selected_members: [{}, {}, {}, {}, {}, {}, {}],     // e.g.:) {'name': '張祐誠', 'number': 22, 'position': 'MB'}
+        }
+      }
+    })
+
+    console.log(wrapper.findAll('td'))
+    let rows = wrapper.findAll('td')
+    
+    // for (let i = 0; i < rows.length; ++i) {
+
+    // }
+    await wrapper.vm.$nextTick();
+    console.log(rows.at(0).text(), rows.at(0).text().length, rows.at(0).text().split(' '))
+    console.log(rows.at(0).find('span').classes()[3])
+    for (let i = 0; i < rows.length; ++i) {
+      i %= 5
+      if (i % 5 == 0) {
+        iconColor = rows.at(0).find('span').classes()[3]
+      } else {
+
+      }
+      console.log(i, rows.at(i).text())
+    }
+    
+  })
+
+  
+})
